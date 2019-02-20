4ec7e0040e4a01de7b47db501c3eed6863ba6658
JJ Hendricks <sales@vgpc.com>
2019-02-20 13:22:42 -0700

    replace device name with paste prompt
    let users know where to paste their device info when they customize it on their own.

--- take-photo-2.bat
+++ take-photo-2.bat
@@ -1,3 +1,3 @@
 cd "%~dp0ffmpeg\bin"
-ffmpeg -f dshow -framerate 5 -pixel_format yuyv422 -video_size 1920x1080 -i video="@device_pnp_\\?\usb#vid_046d&pid_082d&mi_00#6&c3c816e&0&0000#{65e8773d-8f56-11d0-a3b9-00a0c9223196}\global" -vframes 1 "%~dp0\2.jpg"
+ffmpeg -f dshow -framerate 5 -pixel_format yuyv422 -video_size 1920x1080 -i video="paste_your_device_identifier_here" -vframes 1 "%~dp0\2.jpg"
 exit 0
